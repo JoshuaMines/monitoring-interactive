03b9bce7a79e4ab9ddc93e1075caafb0e69570ce
Joshua Mines <minesjoshua@gmail.com>
2021-06-29 12:30:29 -0600

    add

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const Rollbar = require('rollbar')
+const rollbar = new Rollbar({
+  accessToken: 'e207a4ff5d4f4ab0bd3fe758eb5fc9a3',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
 
 app.use(express.json())
 
 
 app.get('/', function(req, res) {
+    rollbar.log('hello world')
     res.sendFile(path.join(__dirname, '/public/index.html'))
 })
+
 
 
 const port = process.env.PORT || 4545;
